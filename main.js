12ce1907277018b34c95c35d41aa40753936ff29
akauangr <akauangr@gmail.com>
2021-12-29 01:02:49 -0300

    Synergy update

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
 Game.registerMod("percentage graph mod", {
 	init: function () {
-		//this function is called as soon as the mod is registered
 		this.insertGraphs();
-
 		let MOD = this;
 
-		//declare hooks here
+		//hooks.
 		Game.registerHook('logic', function () {
 			MOD.updatePercentages();
 		});
-
-		//note: this mod does nothing but show a notification at the bottom of the screen once it's loaded
-		Game.Notify(`Percentage Graph mod loaded!`, '', [16, 5]);
 	},
 	save: function () {
-		//use this to store persistent data associated with your mod
 		return ''
 	},
 	load: function (str) {
-		//do stuff with the string data you saved previously
 	},
 	insertGraphs: function () {
 		Game.ObjectsById.forEach(obj => {
@@ -30,16 +23,64 @@ Game.registerMod("percentage graph mod", {
 	updatePercentages: function () {
 		Game.ObjectsById.forEach(obj => {
 			var perc = this.getTotalPercentage(obj);
+			var percSynergy = this.getSynergyPercentage(obj);
 			var percDiv = obj.l.getElementsByClassName("content")[0].getElementsByClassName("percentage")[0];
 			var graphDiv = obj.l.getElementsByClassName("content")[0].getElementsByClassName("graph")[0];
 
-			percDiv.innerHTML = '<b>' + Beautify(perc, 1) + '%</b>';
+			percDiv.innerHTML = '<b>' + Beautify(perc, 1) + '% <span style="font-size: smaller">(+' + Beautify(percSynergy, 1) + '%)</span></b>';
 			graphDiv.innerHTML = this.graphByPerc(perc);
 		});
 	},
 	getTotalPercentage: function (me) {
 		var percCPS = Game.cookiesPs > 0 ? ((me.amount > 0 ? ((me.storedTotalCps * Game.globalCpsMult) / Game.cookiesPs) : 0) * 100) : 0;
 		return percCPS;
+	},
+	getSynergyPercentage: function (me){
+		if (me.amount>0)
+		{
+			var synergiesWith={};
+			var synergyBoost=0;
+			
+			if (me.name=='Grandma')
+			{
+				for (var i in Game.GrandmaSynergies)
+				{
+					if (Game.Has(Game.GrandmaSynergies[i]))
+					{
+						var other=Game.Upgrades[Game.GrandmaSynergies[i]].buildingTie;
+						var mult=me.amount*0.01*(1/(other.id-1));
+						var boost=(other.storedTotalCps*Game.globalCpsMult)-(other.storedTotalCps*Game.globalCpsMult)/(1+mult);
+						synergyBoost+=boost;
+						if (!synergiesWith[other.plural]) synergiesWith[other.plural]=0;
+						synergiesWith[other.plural]+=mult;
+					}
+				}
+			}
+			else if (me.name=='Portal' && Game.Has('Elder Pact'))
+			{
+				var other=Game.Objects['Grandma'];
+				var boost=(me.amount*0.05*other.amount)*Game.globalCpsMult;
+				synergyBoost+=boost;
+				if (!synergiesWith[other.plural]) synergiesWith[other.plural]=0;
+				synergiesWith[other.plural]+=boost/(other.storedTotalCps*Game.globalCpsMult);
+			}
+			
+			for (var i in me.synergies)
+			{
+				var it=me.synergies[i];
+				if (Game.Has(it.name))
+				{
+					var weight=0.05;
+					var other=it.buildingTie1;
+					if (me==it.buildingTie1) {weight=0.001;other=it.buildingTie2;}
+					var boost=(other.storedTotalCps*Game.globalCpsMult)-(other.storedTotalCps*Game.globalCpsMult)/(1+me.amount*weight);
+					synergyBoost+=boost;
+					if (!synergiesWith[other.plural]) synergiesWith[other.plural]=0;
+					synergiesWith[other.plural]+=me.amount*weight;
+				}
+			}
+		}
+		return (synergyBoost/Game.cookiesPs)*100
 	},
 	graphByPerc: function (perc) {
 		var graphNumber = 0;
@@ -53,37 +94,7 @@ Game.registerMod("percentage graph mod", {
 		if (this.dir) {
 			return "<img src='" + this.dir + "/graph/" + graphNumber + ".png' />"
 		} else {
-			return "<img src='img/money.png' />";
+			return "";
 		}
 	}
 });
-
-/*
-
-Breakdown of info.txt:
-	"Name": "Sample Mod",
-		the visibly displayed name of your mod
-	"ID": "sample mod",
-		the text id of your mod (alphanumeric characters and spaces only), used as its key when saving or loading data
-		also used as identifiers in other mods' dependencies
-		this id must be the same as the first parameter used in your Game.registerMod(id,hooks)
-	"Author": "Orteil",
-		your name here!
-	"Description": "A bare-bones sample mod.",
-		a description of your mod
-	"ModVersion": 1,
-		a number value for your mod's version
-	"GameVersion": 2.031,
-		the last version of Cookie Clicker this mod was confirmed to run on
-	"Date": "13/08/2021",
-		the date your mod was released or last updated
-	"Dependencies": [],
-		optional; an array of IDs of other mods that must be loaded before this one, ie. ["cool mod preloader","extra stuff"]
-	"LanguagePacks": ["lang.js"],
-		optional; an array of local files containing localization data (ie. changing game text, adding translations etc)
-	"Disabled": 1,
-		optional; if set to 1, this mod will be disabled by default
-	"AllowSteamAchievs":1,
-		optional; by default, mods (unless they only consist of language files) block the unlocking of Steam achievements while enabled; set this to 1 if this is a good honest mod that does not incredibly unbalance the game
-
-*/
